ff5b72eee8ec5cd192cbe49b82f629613d232adb
naoshiro <oshiro@clann-inc.com>
2020-08-07 11:41:23 +0900

    Fix

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,6 @@
 "use strict";
 exports.__esModule = true;
-var React = require("react");
+var react_1 = require("react");
 var core_1 = require("@material-ui/core");
 var useStyles = core_1.makeStyles(function (theme) { return ({
     root: {
@@ -10,7 +10,7 @@ var useStyles = core_1.makeStyles(function (theme) { return ({
 exports["default"] = (function (_a) {
     var children = _a.children;
     var classes = useStyles();
-    return (React.createElement(core_1.Box, null,
-        React.createElement(core_1.Box, { className: classes.root }, "hoge"),
+    return (react_1["default"].createElement(core_1.Box, null,
+        react_1["default"].createElement(core_1.Box, { className: classes.root }, "hoge"),
         children));
 });
