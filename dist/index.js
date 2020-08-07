11e828e09122dd0548e88c70633c5873e21904ab
naoshiro <oshiro@clann-inc.com>
2020-08-07 11:36:43 +0900

    Fix

--- dist/index.js
+++ dist/index.js
@@ -2,7 +2,15 @@
 exports.__esModule = true;
 var React = require("react");
 var core_1 = require("@material-ui/core");
+var useStyles = core_1.makeStyles(function (theme) { return ({
+    root: {
+        color: theme.palette.primary.main
+    }
+}); });
 exports["default"] = (function (_a) {
     var children = _a.children;
-    return React.createElement(core_1.Box, null, children);
+    var classes = useStyles();
+    return (React.createElement(core_1.Box, null,
+        React.createElement(core_1.Box, { className: classes.root }, "hoge"),
+        children));
 });
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,22 @@
 import * as React from "react";
-import { Box } from "@material-ui/core";
+import { Box, makeStyles } from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    color: theme.palette.primary.main,
+  },
+}));
 
 type Props = {
   children: React.ReactNode;
 };
 
-export default ({ children }: Props) => <Box>{children}</Box>;
+export default ({ children }: Props) => {
+  const classes = useStyles();
+  return (
+    <Box>
+      <Box className={classes.root}>hoge</Box>
+      {children}
+    </Box>
+  );
+};
