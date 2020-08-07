38bdf33e5466bc6942aabfd48916dcb48b7c1fd7
naoshiro <oshiro@clann-inc.com>
2020-08-07 11:44:14 +0900

    Fix

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,9 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 exports.__esModule = true;
-var react_1 = require("react");
+var react_1 = __importDefault(require("react"));
 var core_1 = require("@material-ui/core");
 var useStyles = core_1.makeStyles(function (theme) { return ({
     root: {
