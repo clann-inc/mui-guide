211ee893f6431846fd6b00bb04b0a5c47852357e
naoshiro <oshiro@clann-inc.com>
2020-08-07 11:26:56 +0900

    Fix

---
+++ dist/index.js
@@ -0,0 +1,5 @@
+"use strict";
+exports.__esModule = true;
+var React = require("react");
+var core_1 = require("@material-ui/core");
+exports["default"] = (function () { return React.createElement(core_1.Box, null, "hoge"); });
