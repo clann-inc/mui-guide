3f1013df679972bceae99b6339638f79dd5b2285
naoshiro <oshiro@clann-inc.com>
2020-08-07 11:15:55 +0900

    FIx

--- dist/index.jsx
+++ dist/index.jsx
@@ -1,5 +1,3 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
 // import React from "react";
-const core_1 = require("@material-ui/core");
-exports.default = () => <core_1.Box>hoge</core_1.Box>;
+import { Box } from "@material-ui/core";
+export default () => <Box>hoge</Box>;
